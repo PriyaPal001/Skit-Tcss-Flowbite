18112e52d4b465efc1645d651eb425ef9c524e7b
PriyaPal001 <vu3t1s2021032@pvppcoe.ac.in>
2023-04-28 20:19:51 +0530

    added changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,18 +10,18 @@
 
   <div class="parent">
     <div class="div1">
-      <img
+      <a href="https://kit.svelte.dev"><img
         src="https://raw.githubusercontent.com/sveltejs/branding/master/svelte-logo.svg"
         alt=""
-      />
-      <img
+      /></a>
+      <a href="https://flowbite-svelte.com"><img
         src="https://flowbite-svelte.com/images/flowbite-svelte-icon-logo.svg"
         alt=""
-      />
-      <img
+      /></a>
+      <a href="https://tailwindcss.com"><img
         src="https://camo.githubusercontent.com/5734d0669fe22ce04a1cb989a156cd32c379875f6bca56d5210c9432824856d9/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f7461696c77696e646373732f7461696c77696e646373732d69636f6e2e737667"
         alt=""
-      />
+      /></a>
       <!-- <img
         src="https://as2.ftcdn.net/v2/jpg/03/29/27/69/1000_F_329276955_XDx5H5D9KGlY9ckipnevWMCM3weKOZvl.jpg"
         alt=""
